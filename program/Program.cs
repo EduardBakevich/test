bc19f920ab05e09e7529f7d043d9478a6a94ebfc
EduardBakevich <ed.bakevich@gmail.com>
2023-07-25 15:41:14 +0300

    main method

--- program/Program.cs
+++ program/Program.cs
@@ -4,3 +4,15 @@
 
 string[] initialArray = new string[] {“Hello”, “2”, “world”, “:-)”};
 string[] sortArray = new string[initialArray.Length];
+void Sort (string[] initialArray, string[] sortArray)
+{
+    int count = 0;
+    for (int i = 0; i < initialArray.Length; i++)
+    {
+    if(initialArray[i].Length <= 3)
+        {
+        sortArray[count] = initialArray[i];
+        count++;
+        }
+    }
+}
