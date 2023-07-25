7a7953e0594654f5038a911f3dc773c5e0294f0e
EduardBakevich <ed.bakevich@gmail.com>
2023-07-25 15:38:28 +0300

    two array

---
+++ program/Program.cs
@@ -0,0 +1,6 @@
+﻿/*Написать программу, которая из заданного массива строк 
+([“Hello”, “2”, “world”, “:-)”]) сформирует массив 
+из строк, длина которых меньше либо равна 3 символам.*/
+
+string[] initialArray = new string[] {“Hello”, “2”, “world”, “:-)”};
+string[] sortArray = new string[initialArray.Length];
