0fc0ae4b8f1797cd601f685728ec194d67dcf4c0
EduardBakevich <ed.bakevich@gmail.com>
2023-07-25 15:44:05 +0300

    print result

--- program/Program.cs
+++ program/Program.cs
@@ -16,3 +16,13 @@ void Sort (string[] initialArray, string[] sortArray)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Sort (initialArray, sortArray);
+PrintArray(sortArray);
